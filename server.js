d6fd925ad0dbc9c83ea9d2a61f9e8375f2873291
Sarah Joy Calpo <sajoy.pc@gmail.com>
2018-03-28 22:39:06 -0700

    create schema with type constructors

--- server.js
+++ server.js
@@ -1,24 +1,70 @@
 const express = require('express');
 const expressGraphql = require('express-graphql');
-const { buildSchema } = require('graphql') ;
+const { buildSchema, GraphQLSchema, GraphQLObjectType, GraphQLList, GraphQLString, GraphQLInt } = require('graphql') ;
 const PORT = process.env.PORT || 3000;
+const joinMonster = require('join-monster');
 
 
-// make a graphql schema
-    // the names listed within the Query type are what we can ask for
-    // other Types describe how our models look
-const schema = buildSchema(`
-    type Query {
-        day(id: Int!): Day,
-        days(week: Int, month: Int): [Day]
-    },
-    type Day {
-        id: Int,
-        date: String,
-        week: Int,
-        month: Int
-    }
-`);
+const Day = new GraphQLObjectType ({
+    name: 'Day',
+    sqlTable: 'operations.day',
+    uniqueKey: 'id',
+    fields:  () => ({
+        id: {
+            type: GraphQLInt
+        },
+        date: {
+            sqlColumn: 'day_date',
+            type: GraphQLString
+        },
+        week: {
+            sqlColumn: 'week_id',
+            type: GraphQLInt
+        },
+        month: {
+            sqlColumn: 'month_id',
+            type: GraphQLInt
+        }
+    })
+});
+
+const QueryRoot = new GraphQLObjectType({
+    name: 'Query',
+    fields: () => ({
+        day: {
+            type: Day,
+            resolve: (parent, args, context, resolveInfo) => {
+                console.log(args);
+                return 5;
+            }
+        },
+        days: {
+            type: new GraphQLList(Day),
+            args: {
+                week: {
+                    type: GraphQLInt,
+                    defaultValue: 1,
+                    description: 'what the week'
+                }
+            },
+            resolve: (parent, args, context, resolveInfo) => {
+                console.log(args);
+                if (args.week) {
+                    return days;
+                } else {
+                    return null;
+                }
+            }
+        }
+    })
+});
+
+const schema = new GraphQLSchema({
+    description: 'idk man pls just work',
+    query: QueryRoot
+});
+
+// TODO hook me up to dat posgres ish!
 
 // faux data
 const days = [
@@ -54,26 +100,26 @@ const days = [
     // 2. the values are functions that do the work to get/give the data
     // 3. will be passed to our GraphQL middleware
 
-const root = {
-    day: getDay,
-    days: getDays
-};
+// const root = {
+//     day: getDay,
+//     days: getDays
+// };
 
-function getDay (args) {
-    return days.filter(day => day.id === args.id)[0];
-}
+// function getDay (args) {
+//     return days.filter(day => day.id === args.id)[0];
+// }
 
-function getDays (args) {
-    if (args.week) {
-        return days.filter(day => day.week === args.week);
-    } else if (args.month) {
-        return days.filter(day => day.month === args.month);
-    } else {
-        return days;
-    }
-}
+// function getDays (args) {
+//     if (args.week) {
+//         return days.filter(day => day.week === args.week);
+//     } else if (args.month) {
+//         return days.filter(day => day.month === args.month);
+//     } else {
+//         return days;
+//     }
+// }
 
-
+ 
 
 
 const app = express();
@@ -82,7 +128,6 @@ app.use(express.static('./public'));
 
 app.use('/graphql', expressGraphql({
     schema: schema,
-    rootValue: root,
     graphiql: true
 }));
 
