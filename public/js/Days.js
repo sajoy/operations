190b79d331777a6e816b1889a211e6e694432257
Sarah Joy Calpo <sajoy.pc@gmail.com>
2018-04-09 12:26:34 -0700

    display activities!

--- public/js/Days.js
+++ public/js/Days.js
@@ -1,15 +1,56 @@
 class Day {
     constructor (data) {
         Object.keys(data).map(propName => this[propName] = data[propName]);
+
+        this.setDisplayInfo();
         this.render();
     }
 
     render () {
         const calendar = document.querySelector('#calendar');
-        const template = document.querySelector('#day-template');
+        const template = document.querySelector('#day-template');       
+        const day = document.importNode(template.content, true);
 
-        
+        day.querySelector('header h2').textContent = this.displayDate;
+        day.querySelector('header h1').textContent = this.displayName;
 
+        const activities = day.querySelector('[data-content="tasks"] ul');
+        const activityTemplate = document.querySelector('#activity-template');
+
+        this.activities.forEach(activity => {
+            // set innerHTML so can setAttribute
+            const ele = document.createElement('li');
+            ele.innerHTML = activityTemplate.innerHTML;
+
+            ele.setAttribute('data-category', activity.category);
+            ele.innerHTML = activity.description;
+
+            activities.appendChild(ele);
+        });
+
+        calendar.prepend(day);
+
+        // grab element and update attributes because you can't when its a document fragment
+        this.ele = calendar.children[0];
+        this.ele.setAttribute('data-week', this.week);
+        this.ele.setAttribute('data-month', this.month);
+    }
+
+    setDisplayInfo () {
+        let dateInfo = new Date(this.date);
+
+        const days = [
+            'Sunday',
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday'
+        ];
+
+        this.displayDate = `${dateInfo.getMonth() + 1}/${dateInfo.getDate()}`;
+        this.displayName = days[dateInfo.getDay()];
     }
 }
 
--- public/js/app.js
+++ public/js/app.js
@@ -45,15 +45,6 @@ class App {
     }
 
 
-    
-//     fetch('https://1jzxrj179.lp.gql.zone/graphql', {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json' },
-//         body: JSON.stringify({ query: '{ posts { title } }' }),
-//   })
-//     .then(res => res.json())
-//     .then(res => console.log(res.data));
-
     fetchDays () {
         this.days = new Days();
         const dataEndPoint = 'http://localhost:3000/graphql'
@@ -65,6 +56,7 @@ class App {
                     days {
                         date
                         week
+                        month
                         activities {
                             description
                             category
